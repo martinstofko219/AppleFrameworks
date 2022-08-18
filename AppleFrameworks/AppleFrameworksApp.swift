a65498a4428ffdd795e8fca673653f230333ddb1
Martin Stofko <martinstofko114@gmail.com>
2022-08-18 14:28:03 -0700

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Martin Stofko on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
